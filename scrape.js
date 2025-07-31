46ae88cd199fe91d03e455530a64af7181c3be13
lizzardie <lizzard.dh@gmail.com>
2025-07-31 14:14:37 -0700

    scraper update

--- scrape.js
+++ scrape.js
@@ -5,11 +5,11 @@ const htmlparse = require('node-html-parser');
 let done = false;
 let allPDFs = [];
 let url = 'https://www.ice.gov/foia/odo-facility-inspections'; // i think if i use this url 
-let aContent = html.querySelectorAll(".field-content > a"); // WORKS, this gets me the entirety of the <a> text content
+let html = getHTML(url); // raw html in string from ky
+let aContent = html.querySelectorAll(".field-content > a"); // WORKS, this gets me the entirety of the <a> text content, can use html instead of document right?
 while(!done){ // while done = false
-    let html = getHTML(url); // raw html in string from ky
     let pdfs = document.querySelector(".field-content > a").getAttribute("href"); // can i use "html" instead of "document" and the aContent variable instead of ".field-content > a" in let pdf?
-    let allPDFs = Array.from(pdfs); // not sure if im able to use the nodelist of let aContent above as is or if i need to convert the let pdfs into array?
+    let allPDFs = Array.from(pdfs); // do i need to convert the let pdfs into array? or is that already done with let allPDFs and the allPDFs thing below
     // also not sure where htmlparse goes in heree (is it before or after the querySelectorAll stuff?)
     if (!aContent.length){ 
         done = true;
